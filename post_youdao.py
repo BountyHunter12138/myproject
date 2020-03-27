c9b99e79bdf79c53a87571e7117099ae23a6017b
BountyHunter12138 <761670147@qq.com>
2020-03-27 15:09:38 +0800

    作业2 12 季繁 add fanyi.min.js.file

--- post_youdao.py
+++ post_youdao.py
@@ -16,30 +16,7 @@ form_data={
     'keyfrom': 'fanyi.web',
     'action': 'FY_BY_REALTlME',
 }
-requests.post(url,form_data)
-
-
-
-
-
-
-
-
-
-
-    'i': '我爱你',
-    'from': 'AUTO',
-    'to': 'AUTO',
-smartresult: dict
-client: fanyideskweb
-salt: 15846865666505
-    'sign': 'ebbca201bf1eaa2797175df91e7dc725',
-ts: 1584686566650
-bv: ec579abcd509567b8d56407a80835950
-doctype: json
-version: 2.1
-keyfrom: fanyi.web
-action: FY_BY_REALTlME
-
+response=requests.post(url,form_data)
+print(response.text)
 
 
