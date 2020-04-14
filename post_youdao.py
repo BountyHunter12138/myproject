ad1b82d9bfeb938fc05ec8f7081a9b2134e813e9
BountyHunter12138 <761670147@qq.com>
2020-04-14 09:55:00 +0800

    作业6--12--jifan finish post data

--- post_youdao.py
+++ post_youdao.py
@@ -1,77 +1,71 @@
-from random import random
 import random
 import requests
 import time
+import hashlib
+import json
 
-url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
-content = " 我和你 "
+from requests import Response
 
-def get_salt():
-    s=str(random.randint(0,10))
-    t=get_ts()
-    #print("random = ",s)
-    #print("ts =",t)
-    #print("salt =",t+s)
-    return t+s
-    #return '15846865666505'
+class Youdao():
+    def __init__(self,content):
+        self.content=content
+        self.url='http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+        self.ts=self.get_ts()
+        self.salt=self.get_salt()
+        self.sign=self.get_sign()
 
-def get_md5(value):
-    import hashlib
-    m = hashlib.md5()
-    m.update(value.encode("utf-8"))
-    return m.hexdigest()
+    def get_salt(self):
+        return self.ts + str(random.randint(0, 10))
+
+    def get_md5(self,value):
+        m=hashlib.md5()
+        m.update(value.encode("utf-8"))
+        return m.hexdigest()
+
+    def get_sign(self):
+        s="fanyideskweb"+self.content+self.salt+"Nw(nmmbP%A-r6U3EUn]Aj"
+        return self.get_md5(s)
 
 
-def get_sign():
-    e = get_content()
-    i = get_salt()
-    s = "fanyideskweb" + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
-    print("s=", get_md5(s))
-    return get_md5(s)
-    #return 'ebbca201bf1eaa2797175df91e7dc725'
-            #ec579abcd509567b8d56407a80835950
-
-def get_ts():
-    import time
-    t = time.time()
-    ts = str(int(round(t * 1000)))
-    print(ts)
-    return ts
-           #1585615766651
-def get_content():
-    return content
-
-form_data={
-    'i': '我爱你',
-    'from': 'AUTO',
-    'to': 'AUTO',
-    'smartresult': 'dict',
-    'client': 'fanyideskweb',
-    'salt': get_salt(),
-    'sign': get_sign(),
-    'ts': get_ts(),
-    'bv': 'ec579abcd509567b8d56407a80835950',
-    'doctype': 'json',
-    'version': '2.1',
-    'keyfrom': 'fanyi.web',
-    'action': 'FY_BY_REALTlME',
-}
-
-def get_headers():
-    headers={
-        'Cookie: OUTFOX_SEARCH_USER_ID=518732761@10.108.160.18;'
-        'Referer: http://fanyi.youdao.com/'
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400'
-    }
-    return headers
+    def get_ts(self):
+        t = time.time()
+        return str(int(round(t * 1000)))
 
 
 
+    def yield_form_data(self):
+        return {
+            'i': self.content,
+            'from': 'AUTO',
+            'to': 'AUTO',
+            'smartresult': 'dict',
+            'client': 'fanyideskweb',
+            'sign': self.sign,
+            'salt': self.salt,
+            'ts': self.ts,
+            'bv': '70244e0061db49a9ee62d341c5fed82a',
+            'doctype': 'json',
+            'version': '2.1',
+            'keyfrom': 'fanyi.web',
+            'action': 'FY_BY_REALTlME',
+        }
 
-if __name__ == '__main__':
-    print(form_data)
-    print(get_headers())
-    response = requests.post(url,data=form_data,headers=get_headers())
-    print(response.text)
+    def yield_headers(self):
+        return{
+            'Cookie': 'OUTFOX_SEARCH_USER_ID=518732761@10.108.160.18; OUTFOX_SEARCH_USER_ID_NCOO=168453912.11179173; JSESSIONID=aaafKk0ImFZPOcqIWd0fx; ___rl__test__cookies=1586790084730',
+            'Referer': 'http: // fanyi.youdao.com /',
+            'User-Agent': 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        }
 
-都是狗屁
+
+
+    def fanyi(self):
+        response = requests.post(self.url, data=self.yield_form_data(), headers=self.yield_headers())
+        content=json.loads(response.text)
+        return content['translateResult'][0][0]['tgt']
+
+if __name__ =="__main__":
+    while(True):
+        a = input('input:')
+        youdao=Youdao(a)
+        print("fanyi_result :",youdao.fanyi())
