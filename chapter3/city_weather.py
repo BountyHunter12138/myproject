a8fa585f635bd10e12ad29f72854b69a0cf8acaa
BountyHunter12138 <761670147@qq.com>
2020-05-12 15:55:56 +0800

    finish today weather

--- chapter3/city_weather.py
+++ chapter3/city_weather.py
@@ -4,21 +4,23 @@ import requests
 class HeFeng():
     def __init__(self):
         self.url = "https://cdn.heweather.com/china-city-list.txt"
-
+        self.encoding='utf8'
+        self.pre_request="https://free-api.heweather.net/s6/weather/now?=location="
+        self.sub_request="&key=fe25a971118e422ebe08446900d10b04"
     def get_weather(self,city_code):
-        url="https://free-api.heweather.net/s6/weather/now?"+city_code+"&key=fe25a971118e422ebe08446900d10b04"
+        url=self.pre_request+city_code+self.sub_request
         info=requests.get(url)
-        info.encoding='utf8'
+        info.encoding=self.encoding
         print(info.text)
     def get_city_code(self):
-        cities=self.get_citys()
+        cities = self.get_citys()
         for each in cities:
             yield each[2:13]
 
 
     def get_citys(self):
         html=requests.get(self.url)
-        html.encoding='utf8'
+        html.encoding=self.encoding
         cities=html.text.split('\n')
         return cities[6:]
 
@@ -27,5 +29,6 @@ class HeFeng():
 if __name__ == '__main__':
     hefeng = HeFeng()
     codes=hefeng.get_city_code()
-    hefeng.get_weather(codes.__next__())
+    for i in range(10):
+        print(hefeng.get_weather(codes.__next__()))
 
